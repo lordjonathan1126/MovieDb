909033c03513611ec07ea22fcef44ce2ed88a738
Jonathan Ng <iamjonathanng6657@gmail.com>
2020-10-28 16:30:25 +0800

    Initial Commit

---
+++ MovieDb/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieDb
+//
+//  Created by Jonathan Ng on 28/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
